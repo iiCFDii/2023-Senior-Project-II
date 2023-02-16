bdf95de552c7de4884df1197fc9bde84f1934556
Christopher Ciobanu <81142363+iiCFDii@users.noreply.github.com>
2023-02-15 20:33:12 -0800

    Added Coordinate and Center Detection for Drone Landing

--- Landing-Sequence/aprilTag_Video.py
+++ Landing-Sequence/aprilTag_Video.py
@@ -3,15 +3,32 @@ import argparse
 import cv2
 import os
 import numpy as np 
-# os.environ['DISPLAY'] = ':0'
+os.environ['DISPLAY'] = ':0'
 # construct the argument parser and parse the arguments
 # ap = argparse.ArgumentParser()
 # ap.add_argument("-i", "--image", required=True,
 # 	help="path to input image containing AprilTag")
 # args = vars(ap.parse_args())
-print("test")
+#print("test")
 vid = cv2.VideoCapture(0)
 kernel = np.ones((5, 5), np.uint8)
+width = vid.get(3)
+height = vid.get(4)
+fps = vid.get(5)
+
+cx = int(width/2)
+cy = int(height/2)
+
+cxPlus = cx + 10
+cyPlus = cy + 10
+
+cxNeg = cx - 10
+cyNeg = cy - 10
+
+
+print(width)
+print(height)
+
 while(True):
     # load the input image and convert it to grayscale
     # print("[INFO] loading image...")
@@ -21,6 +38,10 @@ while(True):
         break
     # cv2.imshow("Image", image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # moment = cv2.moments(gray)
+    # X = int(moment ["m10"] / moment["m00"])
+    # Y = int(moment ["m01"] / moment["m00"])
+    cv2.circle(image, (cx, cy), 10, (0, 0, 200), 2)
     cv2.imshow("Image", image)
 
     # define the AprilTags detector options and then detect the AprilTags
@@ -59,6 +80,21 @@ while(True):
         # draw the center (x, y)-coordinates of the AprilTag
         (cX, cY) = (int(r.center[0]), int(r.center[1]))
         cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
+        if (cX >= cxNeg and cX <= cxPlus) and (cY >= cyNeg and cY <= cyPlus):
+            cv2.circle(image, (cx, cy), 10, (0, 200, 0), 2)
+            print("AprilTag is Centered")
+        elif (cX < cxNeg and cY < cyNeg):
+            print("Move Drone forward and right")
+        elif (cX < cxNeg and cY > cyPlus):
+            print("Move Drone backwards and right")
+        elif (cX > cxPlus and cY > cyPlus):
+            print("Move Drone backwards and left")
+        elif (cX > cxPlus and cY < cyNeg):
+            print("Move Drone forwards and left")
+        else:
+            print("Unknown Drone location")
+
+ 
         # draw the tag family on the image
         tagFamily = r.tag_family.decode("utf-8")
         cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15),
